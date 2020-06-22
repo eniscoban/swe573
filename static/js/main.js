53083952251705da53d2c35b4a7eaa85100f324b
EnisCoban <eniscoban@gmail.com>
2020-06-22 16:42:55 +0300

    csrftoken check

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,22 @@
 var mainjs = {
 
+    getCookie: function(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    },
+
+
     validateEmail: function (email) {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
@@ -90,6 +107,10 @@ var mainjs = {
             let csrftoken = Cookies.get('csrftoken');
 
             console.log("csrftoken:"+csrftoken);
+
+            var csrftoken1 = this.getCookie('csrftoken');
+            console.log("csrftoken1:"+csrftoken);
+
 
             $.ajax({
             url: '/signup/',
